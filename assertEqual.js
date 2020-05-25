35a676510e0e704cee6d447ed70d187f59303c0c
alex-hladun <alexhladun@gmail.com>
2020-05-25 21:41:04 +0000

    Updated scripts.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
   } else {
-    console.log("🛑🛑🛑" + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
   }
   // console.assert(actual === expected,"🛑🛑🛑" + actual + " !== " + expected);
 };
@@ -11,3 +11,4 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("wth", "wth");
 
+assertEqual(2,2)
