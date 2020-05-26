df99413c384306560dfe4ac50071463f39984a09
alex-hladun <alexhladun@gmail.com>
2020-05-26 19:26:03 +0000

    Added eqarrays.js which returns true if both arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false
+    // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
+  } else {
+    for (let i = 0; i <= actual.length - 1; i++) {
+      if (actual[i] !== expected[i]) {
+        return false
+        // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
+      }
+    }
+    return true
+  }
+
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true);
