17a505b04c26e0a8418a7cd11d29190321e8e07a
alex-hladun <alexhladun@gmail.com>
2020-05-29 19:36:05 +0000

    Added suport for nested arrays to eqArrays.js.

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
   } else {
@@ -8,19 +8,29 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
-    return false
-    // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
+    return false;
   } else {
-    for (let i = 0; i <= actual.length - 1; i++) {
-      if (actual[i] !== expected[i]) {
-        return false
-        // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i].constructor === Array && expected[i].constructor === Array) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
+        return false;
       }
     }
-    return true
+    return true;
   }
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+
+let arrA = [4, [5, 6, [5, 4, [4, 3, 2, [1]]]]];
+let arrB = [4, [5, 6, [5, 4, [4, 3, 2, [1]]]]];
+assertEqual(eqArrays(arrA, arrB), true);
