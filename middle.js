e80607018bf5fb03ed62b961850c98b8c541b66a
alex-hladun <alexhladun@gmail.com>
2020-06-01 19:00:44 +0000

    Added testing support for middle.js in assertArraysEqualTest.js

--- middle.js
+++ middle.js
@@ -1,34 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
-    return false;
-  } else {
-    for (let i = 0; i <= actual.length - 1; i++) {
-      if (actual[i] !== expected[i]) {
-        console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
-        return false;
-      }
-    }
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-    return true;
-  }
-};
-
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-    // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
-  } else {
-    for (let i = 0; i <= actual.length - 1; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-        // return `🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`;
-      }
-    }
-    return true;
-  }
-};
-
 const middle = function(array) {
   let middleArray = [];
   let middleIndex = Math.floor(array.length/2);
@@ -44,9 +13,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([6, 9]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,13 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const assertEqual = require('../assertEqual');
+const middle = require('../middle');
 
 assertEqual(assertArraysEqual([4, 3, 2], [3, 2, 3]), false);
 assertEqual(assertArraysEqual([4, 3, 2], [4, 3, 2]), true);
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([6, 9]), []);
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
